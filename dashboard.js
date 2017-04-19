11edca38f2cce070b6feb95e9c759424e587330c
Robert Kubis <rkubis@google.com>
2017-04-19 14:54:14 +0100

    removed unneccessary code
    Change-Id: I1f37102c9a0b2618fe32d5a99dd0d00d182ea86c

--- dashboard.js
+++ dashboard.js
@@ -159,13 +159,6 @@ app.controller('ProjectTopicsController', function($scope, $timeout, projectsSer
         pubsub.projects.subscriptions
             .delete({ subscription: 'projects/' + projectId + '/subscriptions/' + PUBSUB_SUBSCRIPTION_NAME })
             .then(
-                function(response) {
-                    if (response.status != 200 && response != 404) {
-                        console.log(response)
-                    }
-                }
-            )
-            .then(
                 createTopicPrFn, createTopicPrFn
             ).then(
                 function(response) {
@@ -192,13 +185,6 @@ app.controller('ProjectTopicsController', function($scope, $timeout, projectsSer
         };
         pubsub.projects.subscriptions
             .delete({ subscription: 'projects/' + projectId + '/subscriptions/' + PUBSUB_SUBSCRIPTION_NAME })
-            .then(
-                function(response) {
-                    if (response.status != 200 && response != 404) {
-                        console.log(response)
-                    }
-                }
-            )
             .then(clearStatusFn, clearStatusFn);
     };
 
